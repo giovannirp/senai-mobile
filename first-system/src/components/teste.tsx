cb3861cb9b1ea2a062d4ee3390496db059f9b236
Giovanni <giovanni_ge@hotmail.com>
2024-09-09 10:20:12 -0300

    feat: add

---
+++ first-system/src/components/teste.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MeuFormulario: React.FC = () => {
+  return (
+    <form>
+      {/* Campos do formulário aqui */}
+    </form>
+  );
+};
+
+export default MeuFormulario;
