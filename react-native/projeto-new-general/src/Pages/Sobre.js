f989e20ac81c04bab8a5bc654e46831613c1c5ca
giovanni <giovanni_ge@hotmai.com>
2024-10-20 20:34:59 -0300

    feat: add

--- react-native/projeto-new-general/src/Pages/Sobre.js
+++ react-native/projeto-new-general/src/Pages/Sobre.js
@@ -1,19 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, Alert, StyleSheet, ScrollView } from 'react-native';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { View, Text, Alert, StyleSheet, ScrollView } from "react-native";
+import axios from "axios";
 
 export default function Sobre() {
   const [contatos, setContatos] = useState([]);
 
   // Função para buscar contatos do servidor
   const fetchContatos = () => {
-    axios.get('http://10.0.2.2:3000/contatos')
-      .then(resposta => {
+    axios
+      .get("http://10.0.2.2:3000/contatos")
+      .then((resposta) => {
         setContatos(resposta.data);
       })
-      .catch(error => {
-        console.error('Erro ao buscar contatos:', error);
-        Alert.alert('Erro', 'Não foi possível conectar ao servidor.');
+      .catch((error) => {
+        console.error("Erro ao buscar contatos:", error);
+        Alert.alert("Erro", "Não foi possível conectar ao servidor.");
       });
   };
 
@@ -25,21 +26,20 @@ export default function Sobre() {
   // Renderiza a lista de contatos
   return (
     <ScrollView>
-<View style={styles.container}>
-      <Text style={styles.title}>Contatos</Text>
-      {contatos.length > 0 ? (
-        contatos.map((contato, index) => (
-          <View key={index} style={styles.contatoItem}>
-            <Text style={styles.contatoNome}>{contato.nome}</Text>
-            <Text>{contato.telefone}</Text>
-          </View>
-        ))
-      ) : (
-        <Text style={styles.noContacts}>Nenhum contato disponível.</Text>
-      )}
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.title}>Contatos</Text>
+        {contatos.length > 0 ? (
+          contatos.map((contato, index) => (
+            <View key={index} style={styles.contatoItem}>
+              <Text style={styles.contatoNome}>{contato.nome}</Text>
+              <Text>{contato.telefone}</Text>
+            </View>
+          ))
+        ) : (
+          <Text style={styles.noContacts}>Nenhum contato disponível.</Text>
+        )}
+      </View>
     </ScrollView>
-    
   );
 }
 
@@ -51,21 +51,21 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
   },
   contatoItem: {
     padding: 10,
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+    borderBottomColor: "#ccc",
   },
   contatoNome: {
     fontSize: 18,
   },
   noContacts: {
     fontSize: 16,
-    color: 'gray',
-    textAlign: 'center',
+    color: "gray",
+    textAlign: "center",
     marginTop: 20,
   },
 });
